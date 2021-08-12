2df780c2c726a10ea88c9ade469288d0e1753819
Pedro Arthur <54337976+Pedroor@users.noreply.github.com>
2021-08-12 02:18:57 -0300

    fix: reactotron

--- src/config/reactotron.js
+++ src/config/reactotron.js
@@ -4,7 +4,7 @@ import { reactotronRedux } from "reactotron-redux";
 import sagaPlugin from "reactotron-redux-saga";
 
 Reactotron.setAsyncStorageHandler(AsyncStorage)
-  .configure({ host: "192.168.40.110" })
+  .configure({ host: "" })
   .useReactNative()
   .use(reactotronRedux())
   .use(sagaPlugin())
